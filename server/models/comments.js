c6352ff7ea74e0a434c3c64ab443eab6f4f9d8d5
Alex <derputyd3rp@gmail.com>
2025-04-17 18:34:46 -0400

    models

--- server/models/comments.js
+++ server/models/comments.js
@@ -1,1 +1,16 @@
 // Comment Document Schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const commentsSchema = new Schema({
+    content: {type: String, required: true, maxLength: 500},
+    commentIDs: [{type: mongoose.Schema.Types.ObjectId, ref: 'Comment'}],
+    commentedBy: {type: String, required: true},
+    commentedDate: {type: Date, default: () => Date.now()}
+});
+
+commentsSchema.virtual('url').get(function() {
+    return 'comments/' + this._id;
+});
+
+module.exports = mongoose.model('Comment', commentsSchema);
--- server/models/linkflairs.js
+++ server/models/linkflairs.js
@@ -1,1 +1,13 @@
 // LinkFlair Document Schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const linkflairsSchema = new Schema({
+    content: {type: String, maxLength: 30}
+})
+
+linkflairsSchema.virtual('url').get(function () {
+    return 'linkFlairs/' + this._id;
+});
+
+module.exports = mongoose.model('LinkFlair', linkflairsSchema)
